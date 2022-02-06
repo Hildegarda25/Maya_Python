368e9aaf0c6b860af6114c3ba06a94b854fda334
Hildegarda25 <farahmasiell28@gmail.com>
2022-02-06 02:02:25 -0500

    maya_python18

---
+++ Python/tabla03.py
@@ -0,0 +1,12 @@
+n1=float(input("Escribe aqui tu primer numero: "))
+n2=float(input("Escribe tu segundo numero: "))
+n3=float(input("Escribe tu ultimo numero: "))
+n4=float(input("HA dicho el ultimoo: "))
+a=n1*n3/n4
+b=n4-n2/n3*n2
+c=(-n3)*+n4
+d=n3-n1+n4*n2
+print(a)
+print(b)
+print(c)
+print(d)
